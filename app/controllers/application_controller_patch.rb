500fd8b841a3fe400916a55d3c2fc86ee96d99d1
Kiwamu Kato <kato.kiwamu@future.co.jp>
2012-01-10 17:41:34 +0900

    Added NG keyword for adhoc fix to "redundant UTF-8 sequence" in String#to_json

--- app/controllers/application_controller_patch.rb
+++ app/controllers/application_controller_patch.rb
@@ -13,10 +13,20 @@ module AccessLoggerPlugin
     
     module InstanceMethods    
       def access_logging 
-        req_param = params.reject{|k,v| k == "password"}.update({"user" => User.current.login}).to_json
+        p = params.dup
+        p.delete(:password) 
+        p.delete(:password_confirmation) 
+        p.delete(:new_password) 
+        p.delete(:new_password_confirmation) 
+        
+        # ad-hoc fix for "redundant UTF-8 sequence" in String#to_json
+        p.delete(:attachments) 
+        p.delete(:file) 
+
+        req_param = p.update({"user" => User.current.login}).to_json
         user = User.current.login.blank? ? "-" : User.current.login
         message = "#{user} #{req_param}" 
-        log = Logger.new(File.join(RAILS_ROOT, "/log/access.log"), "daily") 
+        log = Logger.new(File.join(RAILS_ROOT, "/log/access.log"), "weekly") 
         log.formatter = Logger::Formatter.new 
         log.info(message) 
         log.close
